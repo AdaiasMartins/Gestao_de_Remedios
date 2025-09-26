53d811bdfd95a830eef96b08b260939cbc0e3e7d
Adaias <adaias.martins@dcx.ufpb.br>
2025-09-26 17:31:04 -0300

    chore: adicionando a estrutura base do projeto

---
+++ src/main/java/ufpb/dsc/gestao_de_remedios/GestaoDeRemediosApplication.java
@@ -0,0 +1,13 @@
+package ufpb.dsc.gestao_de_remedios;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestaoDeRemediosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestaoDeRemediosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ufpb/dsc/gestao_de_remedios/GestaoDeRemediosApplicationTests.java
@@ -0,0 +1,13 @@
+package ufpb.dsc.gestao_de_remedios;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestaoDeRemediosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
