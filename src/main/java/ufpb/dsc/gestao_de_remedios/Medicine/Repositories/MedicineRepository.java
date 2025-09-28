6198e66bc69cd3819311700d2cdcbaaf9ee65b49
Adaias <adaias.martins@dcx.ufpb.br>
2025-09-28 09:29:24 -0300

    feat: adicionando Repository de Medicamento (MedicineRepository)

---
+++ src/main/java/ufpb/dsc/gestao_de_remedios/Medicine/Repositories/MedicineRepository.java
@@ -0,0 +1,8 @@
+package ufpb.dsc.gestao_de_remedios.Medicine.Repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import ufpb.dsc.gestao_de_remedios.Medicine.Models.Medicine;
+
+public interface MedicineRepository extends JpaRepository<Medicine, Long> {
+
+}
