acc56f6ece6136147d54bfe7fdd7b2d2d8184c32
Adaias <adaias.martins@dcx.ufpb.br>
2025-09-29 15:10:25 -0300

    feat: adicionando Controller de Usuário com endpoints CRUD

---
+++ src/main/java/ufpb/dsc/gestao_de_remedios/User/Controllers/UserController.java
@@ -0,0 +1,49 @@
+package ufpb.dsc.gestao_de_remedios.User.Controllers;
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+import ufpb.dsc.gestao_de_remedios.User.DTOs.*;
+import ufpb.dsc.gestao_de_remedios.User.Services.UserService;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService service;
+
+    public UserController(UserService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public UserResponseDTO create(@RequestBody @Valid UserCreateDTO dto) {
+        return service.create(dto);
+    }
+
+    @GetMapping
+    public List<UserResponseDTO> list() {
+        return service.list();
+    }
+
+    @GetMapping("/{id}")
+    public UserResponseDTO get(@PathVariable Long id) {
+        return service.get(id);
+    }
+
+    @PutMapping("/{id}")
+    public UserResponseDTO update(@PathVariable Long id, @RequestBody @Valid UserUpdateDTO dto) {
+        return service.update(id, dto);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
+    }
+}
