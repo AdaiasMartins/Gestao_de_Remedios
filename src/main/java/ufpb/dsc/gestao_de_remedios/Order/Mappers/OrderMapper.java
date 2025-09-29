33982860a2dcb6b59d60695d59883aa04c878ba9
Adaias <adaias.martins@dcx.ufpb.br>
2025-09-29 14:26:26 -0300

    feat: adicionando Mapper de Pedido

---
+++ src/main/java/ufpb/dsc/gestao_de_remedios/Order/Mappers/OrderMapper.java
@@ -0,0 +1,28 @@
+package ufpb.dsc.gestao_de_remedios.Order.Mappers;
+
+import ufpb.dsc.gestao_de_remedios.Order.DTOs.OrderItemResponseDTO;
+import ufpb.dsc.gestao_de_remedios.Order.DTOs.OrderResponseDTO;
+import ufpb.dsc.gestao_de_remedios.Order.Models.Order;
+import java.math.BigDecimal;
+
+public class OrderMapper {
+
+    public static OrderResponseDTO toDto(Order o) {
+        var items = o.getItems().stream().map(i ->
+                new OrderItemResponseDTO(
+                        i.getMedicine().getId(),
+                        i.getMedicine().getName(),
+                        i.getQuantity(),
+                        i.getUnitPrice(),
+                        i.getUnitPrice().multiply(BigDecimal.valueOf(i.getQuantity()))
+                )
+        ).toList();
+        return new OrderResponseDTO(
+                o.getId(),
+                o.getCustomer().getId(),
+                o.getTotal(),
+                o.getCreatedAt(),
+                items
+        );
+    }
+}
